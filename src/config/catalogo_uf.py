d6581e4c3a3b52f19e9e2c2da0929db84a249a08
matsuda <brunohiyane@gmail.com>
2026-06-19 17:34:25 -0300

    juntando metadados + dados

--- src/config/catalogo_uf.py
+++ src/config/catalogo_uf.py
@@ -8,15 +8,43 @@ arquivos = list(Path(RAW_PATH).glob("*.CSV"))
 
 for arquivo in arquivos:
     if '_df_' in arquivo.name.lower():
-        try:
-            df = pd.read_csv(
+        try: #try para pegar os metadados
+            df_meta_dados = pd.read_csv(
                 arquivo,
                 encoding="ISO-8859-1",
                 sep=";",
-                on_bad_lines="skip"
+                nrows=8,
+                header=None,
+                usecols=[0,1]
             )
-            parquet_path = Path(RAW_DF_PATH) / (arquivo.stem + ".parquet")
-            df.to_parquet(parquet_path, engine="pyarrow", index=False)
+
+            metadados = {}
+            for _, linha in df_meta_dados.iterrows():
+                chave = str(linha[0]).replace(":", "").strip()
+                valor = str(linha[1]).strip()
+                metadados[chave] = valor
+            
+            # pega o restante dos dados
+            df_dados = pd.read_csv(
+                arquivo,
+                encoding="ISO-8859-1",
+                sep=";",
+                skiprows=8,
+                header=0,
+                decimal=","
+            )
+
+            df_dados = df_dados.dropna(how='all', axis=1)
+
+            # Inserindo metadados em df_dados
+            for chave, valor in metadados.items():
+                df_dados[chave] = valor
+            
+            colunas_metadados = list(metadados.keys())
+            colunas_dados = [col for col in df_dados.columns if col not in colunas_metadados]
+            df_dados = df_dados[colunas_metadados + colunas_dados]
+
+            df_dados.to_parquet(RAW_DF_PATH/(arquivo.stem+".parquet"), engine="pyarrow", index=False)
 
         except Exception as e:
-            print(f"ERRO em {arquivo.name}: {e}")
+            print(f"Erro em {arquivo.name}: {e}")
